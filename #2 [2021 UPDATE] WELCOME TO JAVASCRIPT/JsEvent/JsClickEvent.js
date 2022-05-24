ebe009c462cb4ad1c5ad3a7a477b7f62daab0134
Kim Hue Mang 김희망 <105429536+KIMHUEMANG@users.noreply.github.com>
2022-05-24 20:45:34 +0900

    Update JsClickEvent.js

--- #2 [2021 UPDATE] WELCOME TO JAVASCRIPT/JsEvent/JsClickEvent.js
+++ #2 [2021 UPDATE] WELCOME TO JAVASCRIPT/JsEvent/JsClickEvent.js
@@ -1,4 +1,3 @@
-
 const title = document.querySelector(".hello h1");
 
 function ClickEvent(){
@@ -13,7 +12,21 @@ function handleMouseClick(){
 function handleMouseLeave(){
     title.innerText = "Mouse is gone";
 }
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowOffline(){
+    alert("Oops.. you haven't wifi");
+}
+function handleWindowOnline(){
+    alert("You have a good WiFi");
+}
 
 title.addEventListener("click", ClickEvent);
 title.addEventListener("mouseenter",handleMouseClick);
 title.addEventListener("mouseleave",handleMouseLeave);
+
+//window
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
