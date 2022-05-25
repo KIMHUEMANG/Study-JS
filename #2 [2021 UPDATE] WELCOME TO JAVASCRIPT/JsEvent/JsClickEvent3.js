a253511dcd1873b7e9e500ebe5fa2e8016395756
Kim Hue Mang 김희망 <105429536+KIMHUEMANG@users.noreply.github.com>
2022-05-25 19:48:51 +0900

    Update JsClickEvent3.js

--- #2 [2021 UPDATE] WELCOME TO JAVASCRIPT/JsEvent/JsClickEvent3.js
+++ #2 [2021 UPDATE] WELCOME TO JAVASCRIPT/JsEvent/JsClickEvent3.js
@@ -2,11 +2,15 @@ const h1 = document.querySelector(".hello h1");
 
 function ClickEvent(){
     let clickedClass = "active";
-    if(h1.classList.contains(clickedClass)){
+  /*  if(h1.classList.contains(clickedClass)){
         h1.classList.remove(clickedClass);
     } else{
         h1.classList.add(clickedClass);
-    }
+    } 
+    이 코드는 아래의 toggle 코드와 같다
+    */
+    
+    h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", ClickEvent);
